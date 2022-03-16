9a28f0f943c907a8d38bea350c52624a52257ab4
oliverahyou <ahyouoliver9@gmail.com>
2022-03-16 18:37:06 +0400

    Intro on objects exercises: start

---
+++ Week_2/Day_2/intro-on-objects-exercise.js
@@ -0,0 +1,20 @@
+let obj_credential = {
+    username: "Henry",
+    password: "lucky"
+}
+let database = [obj_credential];
+
+let newsfeed = [
+    {
+        username: "username1",
+        timeline: "timeline1"
+    }
+    {
+        username: "username2",
+        timeline: "timeline2"
+    }
+    {
+        username: "username3",
+        timeline: "timeline3"
+    }
+]
