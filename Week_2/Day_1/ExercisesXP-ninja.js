2d86d0f21f50866e7fbebf3df9fbb22b1cf1da4d
oliverahyou <ahyouoliver9@gmail.com>
2022-03-17 10:48:52 +0400

    XP Ninja beginning

---
+++ Week_2/Day_1/ExercisesXP-ninja.js
@@ -0,0 +1,39 @@
+5 >= 1
+    // Prediction: True
+    // Actual:
+    console.log(Boolean(5 >= 1));
+    0 === 1
+    // Prediction: False
+    // Actual:
+    
+    4 <= 1
+    // Prediction: False
+    // Actual:
+    
+    1 != 1
+    // Prediction: False
+    // Actual:
+    
+    "A" > "B"
+    // Prediction:
+    // Actual:
+    
+    "B" < "C"
+    // Prediction:
+    // Actual:
+    
+    "a" > "A"
+    // Prediction:
+    // Actual:
+    
+    "b" < "A"
+    // Prediction:
+    // Actual:
+    
+    true === false
+    // Prediction: False
+    // Actual:
+    
+    true != true
+    // Prediction: False
+    // Actual:
