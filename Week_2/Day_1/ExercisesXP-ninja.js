324a85f66557c63327f807ddfbd4c1c078a1c970
oliverahyou <ahyouoliver9@gmail.com>
2022-03-18 01:07:44 +0400

    Exercise 3 completed

--- Week_2/Day_1/ExercisesXP-ninja.js
+++ Week_2/Day_1/ExercisesXP-ninja.js
@@ -51,8 +51,10 @@ true != true
 console.log(Boolean(true != true));
 
 // Exercise 2
+// Part 1
 let str_list = prompt("Please give a list of numbers separated by commas.");
 
+// Part 2
 let num_list = str_list.split(',');
 console.log(num_list);
 let num1 = Number(num_list[0]);
@@ -62,3 +64,50 @@ let sum = num1 + num2 + num3;
 
 console.log(sum);
 
+// EXERCISE 3
+// Part 1
+let sentence = prompt ("Write a sentence with the word 'Nemo'.");
+
+// Part 2 
+console.log(sentence.indexOf("Nemo"));
+
+// Part 3
+console.log("I found Nemo at " + sentence.indexOf("Nemo"));
+
+// Part 4
+if (sentence.indexOf("Nemo") > -1)
+{
+    console.log("I found Nemo at " + sentence.indexOf("Nemo"));
+}
+else 
+{
+    console.log("I can\'t find Nemo");
+}
+
+// EXERCISE 4
+let num = prompt("Please write a number.");
+
+if (num < 2)
+{
+    alert("boom");
+}
+else if (num > 2)
+{
+    alert("b" + ("o" * num) + "m");
+}
+else if (num % 2 === 0)
+{
+    alert("boom!")
+}
+else if (num % 5 === 0)
+{
+    alert("BOOM");
+}
+else if (num % 2 && num % 5)
+{
+    alert("BOOM!");
+}
+else 
+{
+    alert("This is strange. Try with another number please.");
+}
