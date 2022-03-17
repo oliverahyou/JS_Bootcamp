82a8788f1636db19010aaa2e7898d67753f539f1
oliverahyou <ahyouoliver9@gmail.com>
2022-03-17 18:39:51 +0400

    Robot friend completed

--- Week_2/Day_1/ExercisesXP-ninja.js
+++ Week_2/Day_1/ExercisesXP-ninja.js
@@ -1,39 +1,64 @@
+// Exercise 1
+
 5 >= 1
-    // Prediction: True
-    // Actual:
-    console.log(Boolean(5 >= 1));
-    0 === 1
-    // Prediction: False
-    // Actual:
+// Prediction: True
+// Actual: True
+console.log(Boolean(5 >= 1));
     
-    4 <= 1
-    // Prediction: False
-    // Actual:
-    
-    1 != 1
-    // Prediction: False
-    // Actual:
-    
-    "A" > "B"
-    // Prediction:
-    // Actual:
-    
-    "B" < "C"
-    // Prediction:
-    // Actual:
-    
-    "a" > "A"
-    // Prediction:
-    // Actual:
-    
-    "b" < "A"
-    // Prediction:
-    // Actual:
-    
-    true === false
-    // Prediction: False
-    // Actual:
-    
-    true != true
-    // Prediction: False
-    // Actual:
+0 === 1
+// Prediction: False
+// Actual: False
+console.log(Boolean(0 === 1));
+
+4 <= 1
+// Prediction: False
+// Actual:False
+console.log(Boolean(4 <= 1));
+
+1 != 1
+// Prediction: False
+// Actual: False
+console.log(Boolean(1 != 1));
+
+"A" > "B"
+// Prediction: False
+// Actual: False
+console.log(Boolean("A" > "B"));
+
+"B" < "C"
+// Prediction: True
+// Actual: True
+console.log(Boolean("B" < "C"));
+
+"a" > "A"
+// Prediction: True
+// Actual: True
+console.log(Boolean("a" > "A"));
+
+"b" < "A"
+// Prediction: False
+// Actual: False
+console.log(Boolean("b" < "A"));
+
+true === false
+// Prediction: False
+// Actual: False
+console.log(Boolean(true === false));
+
+true != true
+// Prediction: False
+// Actual: False
+console.log(Boolean(true != true));
+
+// Exercise 2
+let str_list = prompt("Please give a list of numbers separated by commas.");
+
+let num_list = str_list.split(',');
+console.log(num_list);
+let num1 = Number(num_list[0]);
+let num2 = Number(num_list[1]);
+let num3= Number(num_list[2]);
+let sum = num1 + num2 + num3;
+
+console.log(sum);
+
---
+++ Week_2/Day_1/LearningLoops.js
@@ -0,0 +1,8 @@
+for (a = 0, a < 16, a++){
+let remainder = a%2;
+if (remainder === 0){
+    console.log(a + " is even");
+}
+else if (remainder === 1){
+    console.log(a + " is odd");
+}
