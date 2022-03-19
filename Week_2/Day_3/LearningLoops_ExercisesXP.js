64eb5c32862aa95a4a2417d3cbcb974c4adf52f3
oliverahyou <ahyouoliver9@gmail.com>
2022-03-19 14:14:16 +0400

    Exercises XP completed

--- Week_2/Day_3/LearningLoops_ExercisesXP.js
+++ Week_2/Day_3/LearningLoops_ExercisesXP.js
@@ -70,5 +70,88 @@ console.log(typeof(number));
 // Part 2
 while (number < 10)
 {
-    prompt("Please enter another number.")
+    number = prompt("Please enter another number.")
 }
+
+// EXERCISE 4
+// Part 1
+let building = {
+    numberOfFloors : 4,
+    numberOfAptByFloor : {
+        firstFloor : 3,
+        secondFloor : 4,
+        thirdFloor : 9,
+        fourthFloor : 2,
+    },
+    nameOfTenants : ["Sarah", "Dan", "David"],
+    numberOfRoomsAndRent:  {
+        sarah: [3, 990],
+        dan :  [4, 1000],
+        david : [1, 500],
+    },
+}
+
+// Part 2
+console.log(building.numberOfFloors);
+
+// Part 3
+let apt1 = building.numberOfAptByFloor.firstFloor;
+let apt3 = building.numberOfAptByFloor.thirdFloor;
+let apt_floor1and3 = apt1 + apt3;
+console.log(apt_floor1and3);
+
+// Part 4
+let numOfRooms = building.numberOfRoomsAndRent.dan[0];
+console.log(numOfRooms);
+
+// Part 5
+let rentDan = building.numberOfRoomsAndRent.dan[1];
+let rentSarah = building.numberOfRoomsAndRent.sarah[1];
+let rentDavid = building.numberOfRoomsAndRent.david[1];
+let sumOfRents = rentSarah + rentDavid;
+
+if (sumOfRents > rentDan)
+{
+    rentDan += 200;
+}
+console.log(rentDan);
+
+// EXERCISE 5
+// Part 1
+let family = 
+{
+    mother: "Kristina",
+    father: "Benjamin",
+    sibling1: "Bob",
+    sibling2: "Oliver",
+    uncle: "Henry",
+    cousin1: "Damien",
+    cousin2: "Dheeksha"
+}
+// Part 2 & 3
+for (let i in family)
+{
+    console.log(i);
+    console.log(family[i]);
+}
+
+// EXERCISE 6
+let details = {
+    my: 'name',
+    is: 'Rudolf',
+    the: 'raindeer'
+  }
+let sentence = " ";
+for (let i in details)
+{
+    console.log(sentence += i + " " + details[i] + " ");
+}
+
+// EXERCISE 7
+// Part 1
+let names = ["Jack", "Philip", "Sarah", "Amanda", "Bernard", "Kyle"]
+let namesAlp = names.sort();
+let secretName = " ";
+for (let i = 0; i < namesAlp.length; i++) {
+    console.log(secretName += namesAlp[i][0]);
+  }
