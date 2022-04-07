5d38a520745ae27f7a65a6826d66df2aa8c004f4
oliverahyou <ahyouoliver9@gmail.com>
2022-04-07 20:02:45 +0400

    Update DailyChallenge.js

--- Week_4_Adv.JS/Day_1/DailyChallenge.js
+++ Week_4_Adv.JS/Day_1/DailyChallenge.js
@@ -1,43 +1,42 @@
 const gameInfo = [
-    {
-      username: "john",
-      team: "red",
-      score: 5,
-      items: ["ball", "book", "pen"]
-    },
-    {
-      username: "becky",
-      team: "blue",
-      score: 10,
-      items: ["tape", "backpack", "pen"]
-    },
-    {
-      username: "susy",
-      team: "red",
-      score: 55,
-      items: ["ball", "eraser", "pen"]
-    },
-    {
-      username: "tyson",
-      team: "green",
-      score: 1,
-      items: ["book", "pen"]
-    },
-   ];
+  {
+    username: "john",
+    team: "red",
+    score: 5,
+    items: ["ball", "book", "pen"]
+  },
+  {
+    username: "becky",
+    team: "blue",
+    score: 10,
+    items: ["tape", "backpack", "pen"]
+  },
+  {
+    username: "susy",
+    team: "red",
+    score: 55,
+    items: ["ball", "eraser", "pen"]
+  },
+  {
+    username: "tyson",
+    team: "green",
+    score: 1,
+    items: ["book", "pen"]
+  },
+];
    
-   let winners = [];
-   let score = 0;
+let winners = [];
+let score = 0;
 
 //  Add exclamation point to each username
-   let usernames = [];
-
-   gameInfo.forEach((player) => {
-    //1 
-    usernames.push(`${player.username}!`);
-    //2 
-    player.score > 5 ? winners.push(player.username) : "below 5";
-    //3 Get total score
-    score += player.score;
+let usernames = [];
+gameInfo.forEach((player) => {
+  //1 
+  usernames.push(`${player.username}!`);
+  //2 
+  player.score > 5 ? winners.push(player.username) : "below 5";
+  //3 Get total score
+  score += player.score;
 
 });
 
