8ee65af8446549aa7de848837b593a71f7cee2a8
oliverahyou <ahyouoliver9@gmail.com>
2022-04-12 15:16:42 +0400

    Create Exercise.sql

---
+++ Week_6_Databases/Day_2/Exercise.sql
@@ -0,0 +1,7 @@
+-- SELECT avg(number_oscars) FROM actors;
+
+-- SELECT DISTINCT ON (number_oscars) number_oscars, first_name, last_name FROM actors;
+
+-- SELECT * FROM actors WHERE age > '01/01/1970'
+
+-- SELECT * FROM actors WHERE first_name in ('David', 'Morgan', 'Will');
