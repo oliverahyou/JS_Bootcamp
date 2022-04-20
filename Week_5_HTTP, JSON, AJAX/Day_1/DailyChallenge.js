86804a5eeb64d568bd36f96fe4c78908c5187c3d
oliverahyou <ahyouoliver9@gmail.com>
2022-04-20 22:24:09 +0400

    20 April 22:24 Daily Challenge submit

--- Week_5_HTTP, JSON, AJAX/Day_1/DailyChallenge.js
+++ Week_5_HTTP, JSON, AJAX/Day_1/DailyChallenge.js
@@ -1,5 +1,5 @@
-function myFunction(ev){
-    ev.preventDefault();
+function myFunction(event){
+    event.preventDefault();
     firstname = document.forms.challenge.firstname.value;
     lastname = document.forms.challenge.lastname.value;
     console.log(JSON.stringify({firstname, lastname}));
