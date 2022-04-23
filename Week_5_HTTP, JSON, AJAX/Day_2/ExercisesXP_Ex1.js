3a694c9abb0c82445edaef04db5e8b0f1db2082d
oliverahyou <ahyouoliver9@gmail.com>
2022-04-23 13:54:38 +0400

    23 April 13:54 Exercise XP complete

--- Week_5_HTTP, JSON, AJAX/Day_2/ExercisesXP_Ex1.js
+++ Week_5_HTTP, JSON, AJAX/Day_2/ExercisesXP_Ex1.js
@@ -1,5 +1,44 @@
+// let xhr = new XMLHttpRequest();
+// let url = `https://api.giphy.com/v1/gifs/search?q=hilarious&rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My`
+// xhr.open("GET", url, true);
+// xhr.responseType = "json";
+// xhr.send();
+// let data= []
+// xhr.onload = function(){
+//     if (xhr.status === 200){
+//         console.log(xhr.response);
+//         data = xhr.response.data;
+//         console.log(data);
+//         display_data(data);
+//     }
+//     else {
+//         console.log(`Error ${xhr.status}: ${xhr.statusText}`);
+//     }
+// }
+// xhr.onprogress = function(event){
+//     if (event.lengthComputable){
+//         alert(`Received ${event.loaded} of ${event.total}`)
+//     }
+// }
+// let body = document.body;
+// function display_data(data){
+//     for (let gif of data){
+//         let div = document.createElement("div");
+//         let title = document.createElement("h3");
+//         let iframe = document.createElement("iframe");
+//         iframe.setAttribute("src", `https://giphy.com/embed/${gif.id}`);
+//         iframe.width = "240px";
+//         title.innerText = gif.title;
+//         title.style.width = "240px"
+
+//         div.appendChild(title);
+//         div.appendChild(iframe);
+//         body.appendChild(div);
+//     }
+// }
+
 let xhr = new XMLHttpRequest();
-let url = `https://api.giphy.com/v1/gifs/search?q=hilarious&rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My`
+let url = `https://api.giphy.com/v1/gifs/search?q=sun&rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My&limit=10`
 xhr.open("GET", url, true);
 xhr.responseType = "json";
 xhr.send();
@@ -30,9 +69,10 @@ function display_data(data){
         iframe.width = "240px";
         title.innerText = gif.title;
         title.style.width = "240px"
-
+// 
         div.appendChild(title);
         div.appendChild(iframe);
         body.appendChild(div);
     }
 }
+
