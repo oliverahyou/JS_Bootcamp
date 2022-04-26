87eef47754d851fcd5d87bcf869c3e48cc313c62
oliverahyou <ahyouoliver9@gmail.com>
2022-04-26 15:59:25 +0400

    26 April 15:59

---
+++ Week_8_REACT/Day_2/components/src/App.js
@@ -0,0 +1,68 @@
+// import logo from './logo.svg';
+import './App.css';
+
+import Sunrise from './Sunrise';
+
+const App = () => {
+
+  return (
+    <div className="box">
+      <h1>In the App.js</h1>
+      <Sunrise />
+    </div>
+  )
+}
+
+export default App;
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+// import Car from './Car';
+
+// const App = () => {
+
+//   const carColors = [
+//     "AliceBlue",
+//     "AntiqueWhite",
+//     "Aqua",
+//     "Aquamarine",
+//     "Azure",
+//     "Beige",
+//     "Bisque",
+//     "Black",
+//     "BlanchedAlmond",
+//     "Blue",
+//     "BlueViolet",
+//     "Brown"
+//   ]
+
+
+//   return (
+//     <div className="box">
+//       <h1>In the App.js</h1>
+//       <Car colors={carColors} />
+//     </div>
+//   )
+// }
+
+// export default App;
+
+export default App;
