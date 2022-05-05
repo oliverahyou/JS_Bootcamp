bf27808234713f99e59353bbfddfbfef1eda6a0f
oliverahyou <ahyouoliver9@gmail.com>
2022-05-05 15:26:39 +0400

    5 May 15:26

---
+++ Week_9_REDUX/Day_2/weeklyapp/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+//A.
+import { Provider } from 'react-redux'
+
+//B.
+import { store } from './store'
+
+//C.
+ReactDOM.render(
+    //pass the store to the Provider
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
