03606c172f483c86a4e399c98a245dfd0b0ba5d7
oliverahyou <ahyouoliver9@gmail.com>
2022-05-16 19:13:48 +0400

    16 May 19:13

--- Week_7_NodeJS/Day_2/ExercisesXP/ex2/server.js
+++ Week_7_NodeJS/Day_2/ExercisesXP/ex2/server.js
@@ -6,7 +6,7 @@ const user = {
 const http = require("http");
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader("Content-Type", "application/json")
+    res.setHeader("Content-Type", "application/json");
     let jsonMessage = JSON.stringify(user);
     res.end(jsonMessage);
 })
