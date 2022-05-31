578957a0dba8f74acba53615f35fc333df438b5a
oliverahyou <ahyouoliver9@gmail.com>
2022-05-31 14:56:58 +0400

    31 May 14:56

--- Week_7_NodeJS/Day_2/DailyChallenge/server.js
+++ Week_7_NodeJS/Day_2/DailyChallenge/server.js
@@ -1,22 +1,23 @@
-const express = require("express");
-const app = express();
-
-app.use(express.static("public"));
-
-const cors = require("cors");
-app.use(cors());
+const exp = require('express');
+const app = exp();
+const port = 3000;
 
 app.get("/aboutMe/:hobby", (req, res) => {
     
     if (isNaN(req.params.hobby)){
-        res.send("Hobby: " + req.params["hobby"]);
+        res.send("Hobby: " + req.params.hobby);
     } else {
         res.send("Error! Hobby can only be a string.");
     }
 });
+app.use(exp.static('public'));
 app.get("/pic", (req, res) => {
-    
+    res.send('<img src="potato.jpeg" alt="Image of a potato">');
 });
-app.listen(3000, () => {
+
+app.get("/form", function(req, res){
+    res.sendFile(__dirname, "/form.html");
+});
+app.listen(port, () => {
     console.log("App listening on port 3000...");
 });
