95cf310ba183d8578a2cab68131625426d604bde
oliverahyou <ahyouoliver9@gmail.com>
2022-06-04 12:20:43 +0400

    4 June 12:20

--- Week_7_NodeJS/Day_1/ExercisesXP_Ninja/ex3/regex.js
+++ Week_7_NodeJS/Day_1/ExercisesXP_Ninja/ex3/regex.js
@@ -1,3 +1,19 @@
-const prompt = require("prompt-sync");
-const fullName = prompt("Please enter your full name.");
-console.log(`Welcome ${fullName}.`);
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('What is your full name ? ', function (fullName) {
+    const str = fullName;
+    const rgx = /^[A-Z][a-z]+\s[A-Z][a-z]+$/g
+    const a = rgx.exec(str);
+    if (a) {
+        console.log(`Fullname: ${fullName}`)
+    } else {
+        console.log("Please make sure that there is only one space between the names and each name starts with a capital letter. Only in letters.")
+    };
+    rl.close();
+});
+
+
