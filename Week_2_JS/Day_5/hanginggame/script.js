027cc95e8ce353ed0afdcbc4a58fc9b8a0b2edc7
oliverahyou <ahyouoliver9@gmail.com>
2022-06-07 11:36:59 +0400

    7 June 11:36

---
+++ Week_2_JS/Day_5/hanginggame/script.js
@@ -0,0 +1,13 @@
+let word = prompt("Player 1: Please enter a word with atleast 8 letters.");
+console.log(word);
+console.log(word.length);
+
+if (word.length < 8) {
+    alert("Word is less than 8 letters. Please retry.");
+    while()
+    let a = prompt("Player 1: Please enter a word with atleast 8 letters.");
+    console.log(a.length);
+} else {
+    let guess = prompt("Player 2: Please enter a letter.");
+}
+
