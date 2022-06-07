9cd60f0847a25681926e0e1a23e6c784de773ad3
oliverahyou <ahyouoliver9@gmail.com>
2022-06-07 16:00:07 +0400

    7 June 16:00

--- Week_7_NodeJS/Day_3/ExercisesXP/ex1/fs.js
+++ Week_7_NodeJS/Day_3/ExercisesXP/ex1/fs.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 fs.readFile("./potato.txt", "utf-8", (err, data) => {
     if (err) {
         console.log(err);
-    }else {
+    } else {
         console.log(data);
     };
 });
