f3943063c7485d4f615dd98b3890ad8b33b3f8b1
oliverahyou <ahyouoliver9@gmail.com>
2022-06-07 19:08:03 +0400

    7 June 19:08

---
+++ Week_7_NodeJS/Day_4/PizzaProject/server.js
@@ -0,0 +1,35 @@
+const exp = require("express");
+const app = exp();
+const port = 3000;
+
+app.use(exp.static('public'));
+
+app.set("view engine", "ejs");
+
+let menu = [
+    {
+        name: "Margarita",
+        price: 10,
+        ingredients: ["Tomato Sauce", "Mozzarella", "Basil"]
+    },
+    {
+        name: "Bianca",
+        price: 13,
+        ingredients: ["Ricotta", "Mozzarella", "Garlic"]
+    },
+    {
+        name: "Etna",
+        price: 14,
+        ingredients: ["Tomato sauce", "Mozzarella", "Anchovies", "Capers", "Olives"]
+    }
+];
+
+app.get("/", (req, res) => {
+    res.render("index", { nameRestaurant: "Papa Pizza", pizzaMenu: menu });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.listen(port, () => { console.log("Listening on port 3000!") });
