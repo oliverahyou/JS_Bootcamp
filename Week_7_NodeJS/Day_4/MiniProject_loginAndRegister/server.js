a967a60df2d720399ff0db54d92bcf2cda68b3d4
oliverahyou <ahyouoliver9@gmail.com>
2022-06-08 23:05:23 +0400

    8 June 23:05

--- Week_7_NodeJS/Day_4/MiniProject_loginAndRegister/server.js
+++ Week_7_NodeJS/Day_4/MiniProject_loginAndRegister/server.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const db = require("knex")({
     client: "pg",
     connection: {
-        host:"127.0.0.1",
+        host:"localhost",
         user: "postgres",
         password: "root",
         database:"login&register",
@@ -15,7 +15,6 @@ const db = require("knex")({
     }
 });
 app.set("db", db);
-
 
 app.use(exp.static("public"));
 
@@ -27,14 +26,16 @@ app.use(cors());
 app.get("/", (req, res) => {
     res.sendFile("index.html", ({ root: path.join(__dirname) }));
 });
+
 app.post("/login", (req, res) => {
-    db("login")
-    .insert
+
 });
 app.post("/register", (req, res) => {
     db("register")
-    .insert({first_name: req.body.first_name , last_name: req.body.last_name, email: req.body.email, username: req.body.username, password: req.body.username, da})
+    .insert({first_name: req.body.first_name , last_name: req.body.last_name, email: req.body.email, username: req.body.username, password: req.body.username, created_date: new Date()})
+    .then(register => {
+        res.send(register);
+    })
     // res.send(req.body);
 });
-
 app.listen(port);
