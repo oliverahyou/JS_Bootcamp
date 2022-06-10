f259232d9b8d4384ec8ad33673633108473b977c
oliverahyou <ahyouoliver9@gmail.com>
2022-06-10 08:37:08 +0400

    10 June 08:37

--- Week_8_REACT/Day_1/exercisesxp_gold/src/Planets.js
+++ Week_8_REACT/Day_1/exercisesxp_gold/src/Planets.js
@@ -1,18 +1,14 @@
 import React from 'react';
 
-const Planets = () => {
-    const planets = ['Mars','Venus','Jupiter','Earth','Saturn','Neptune' ];
+const Planets = (props) => {
+  const planets = ['Mars','Venus','Jupiter','Earth','Saturn','Neptune' ];
   return (
-    <div>
-        <ul class="list-group">
-          <li class="list-group-item">{planets[0]}</li>
-          <li class="list-group-item">{planets[1]}</li>
-          <li class="list-group-item">{planets[2]}</li>
-          <li class="list-group-item">{planets[3]}</li>
-          <li class="list-group-item">{planets[4]}</li>
-          <li class="list-group-item">{planets[5]}</li>
-          <li class="list-group-item">{planets[6]}</li>
-        </ul>
+    <div> 
+      {
+        planets.map((user, i) => {
+          return <li className="list-group-item">{planets[i]}</li>
+        })
+      }
     </div>
   )
 }
