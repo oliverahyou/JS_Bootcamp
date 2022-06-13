4cdd31932f745824eba444c6ae39e3330bf3a260
oliverahyou <ahyouoliver9@gmail.com>
2022-06-13 09:35:16 +0400

    13 June 09:35

--- Week_8_REACT/Day_1/sunrise/src/index.js
+++ Week_8_REACT/Day_1/sunrise/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import HelloWorld from "./Hello"
+import Sunrise from './Sunrise';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+     <Sunrise />
+    {/* <App /> */}
+    {/* <HelloWorld /> */}
   </React.StrictMode>
 );
 
