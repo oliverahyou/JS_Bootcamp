a3b8c81d5f6d2a0267d160102c503228270d2294
oliverahyou <ahyouoliver9@gmail.com>
2022-06-27 14:24:18 +0400

    27 June 14:24

--- Week_8_REACT/Day_2/exercises_xp/src/Forms.js
+++ Week_8_REACT/Day_2/exercises_xp/src/Forms.js
@@ -8,11 +8,17 @@ class Forms extends React.Component {
             username: "",
             age: null,
             errormessage: "",
-            selectedValue: ""
+            selectedValue: "",
+            title: "",
+            author: "",
+            genre: "",
+            year: "",
+            successmessage: ""
         }
         this.mySubmitHandler = this.mySubmitHandler.bind(this);
         this.onInputChange = this.onInputChange.bind(this);
         this.onAgeChange = this.onAgeChange.bind(this);
+        this.bookSubmit = this.bookSubmit.bind(this);
     }
     onInputChange (event) {
         this.setState({username: event.target.value});
@@ -28,13 +34,18 @@ class Forms extends React.Component {
             this.setState({errormessage: "Your age must be a number"});
         }
     }
+    bookSubmit (event) {
+        this.setState({successmessage: "Data sent!"})
+        console.log(`{Title: ${this.state.title}, Author: ${this.state.author}, Genre: ${this.state.genre}, Year: ${this.state.year}.}`)
+        event.preventDefault();
+    }
     render () {
         let header;
         if (this.state.username && this.state.age) {
             header = <h1>Hello {this.state.username} {this.state.age}</h1>
         }
         return (    
-            <div className="form">
+            <div className="forms">
                 <form className="potato" onSubmit={this.mySubmitHandler}>
                 <div>{header}</div>
                 <label htmlFor="name">Enter your name:</label><br/>
@@ -49,6 +60,30 @@ class Forms extends React.Component {
                     <option>Volvo</option>
                     <option>Fiat</option>
                 </select>
+                </form>
+
+                <form className="book" onSubmit={this.bookSubmit}>
+                    <h1>New Book</h1>
+                    <div>{this.state.successmessage}</div>
+                    <label>Title</label>
+                    <input type="text" onChange={(event) => {this.setState({title: event.target.value})}}></input><br/>
+                    <label>Author</label>
+                    <input type="text" onChange={(event) => {this.setState({author: event.target.value})}}></input><br/>
+                    <label>Genre</label>
+                    <input type="text" onChange={(event) => {this.setState({genre: event.target.value})}}></input><br/>
+                    <label>Year Published</label>
+                    <input type="text" onChange={(event) => {this.setState({year: event.target.value})}}></input><br/>
+                    <input type="submit" value="Submit"></input>
+                </form>
+
+                <form className="formThree">
+                    <h1 className="welcome">Welcome!</h1>
+                    <p>Please provide your information below.</p>
+                    <input type="text" placeholder="First Name"></input><br/>
+                    <input type="text" placeholder="Last Name"></input><br/>
+                    <input type="tel" placeholder="Phone Number"></input><br/>
+                    <input type="email" placeholder="Email Address"></input><br/>
+                    <input type="submit" value="Submit"></input>
                 </form>
             </div>
         )
