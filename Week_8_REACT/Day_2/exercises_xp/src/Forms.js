cc38add4ac2f52101ea73a7ee6ce1ff833ea39f0
oliverahyou <ahyouoliver9@gmail.com>
2022-06-27 11:33:41 +0400

    27 June 11:33

--- Week_8_REACT/Day_2/exercises_xp/src/Forms.js
+++ Week_8_REACT/Day_2/exercises_xp/src/Forms.js
@@ -5,26 +5,50 @@ class Forms extends React.Component {
     constructor () {
         super();
         this.state = {
-            username: ""
+            username: "",
+            age: null,
+            errormessage: "",
+            selectedValue: ""
+        }
+        this.mySubmitHandler = this.mySubmitHandler.bind(this);
+        this.onInputChange = this.onInputChange.bind(this);
+        this.onAgeChange = this.onAgeChange.bind(this);
+    }
+    onInputChange (event) {
+        this.setState({username: event.target.value});
+    }
+    mySubmitHandler (event) {
+        this.setState({username: event.target.value});
+        alert(`You are submitting ${this.state.username}.`);
+        event.preventDefault();
+    }
+    onAgeChange (event) {
+        this.setState({age: event.target.value});
+        if (isNaN(this.state.age)){
+            this.setState({errormessage: "Your age must be a number"});
         }
     }
-    onInputChange(event){
-        this.setState({
-            username: event.target.value
-        })
-    }
-    render() {
+    render () {
         let header;
-        if (this.state.username) {
-            header = <h1>Hello {this.state.username}</h1>
+        if (this.state.username && this.state.age) {
+            header = <h1>Hello {this.state.username} {this.state.age}</h1>
         }
         return (    
             <div className="form">
-                <form className="potato">
+                <form className="potato" onSubmit={this.mySubmitHandler}>
                 <div>{header}</div>
-                <p>Enter your name:</p>
-                <input type="text" name="name" onChange={(event) => {this.onInputChange(event)}}></input>
-                <input type="submit" value="Submit"></input>
+                <label htmlFor="name">Enter your name:</label><br/>
+                <input type="text" name="name" onChange={(event) => {this.onInputChange(event)}}></input><br/>
+                <label htmlFor="age">Enter your age:</label><br/>
+                <input type="text" name="age" onChange={(event) => {this.onAgeChange(event)}}></input><br/>
+                <h3>{this.state.errormessage}</h3>
+                <textarea value="The content of a textarea goes in the value attribute"></textarea><br/>
+                <input type="submit" value="Submit" id="submitbtn"></input><br/>
+                <select onChange={(event) => {this.setState({selectedValue: event.target.value})}}>
+                    <option>Ford</option>
+                    <option>Volvo</option>
+                    <option>Fiat</option>
+                </select>
                 </form>
             </div>
         )
